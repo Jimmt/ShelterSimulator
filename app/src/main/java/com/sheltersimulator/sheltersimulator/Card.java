f047153f486d460701e2539237594eaf4e63ef0b
Jimmt <austin.hsieh0@gmail.com>
2019-03-01 00:06:30 -0800

    Added basic start menu and flexible decision UI

---
+++ app/src/main/java/com/sheltersimulator/sheltersimulator/Card.java
@@ -0,0 +1,140 @@
+package com.sheltersimulator.sheltersimulator;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Space;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Card.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Card#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Card extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String DECISION = "arg_decision";
+
+    // TODO: Rename and change types of parameters
+    private Decision decision;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Card() {
+        // Required empty public constructor
+    }
+
+
+    public static Card newInstance(Decision decision) {
+        Card fragment = new Card();
+        Bundle args = new Bundle();
+        args.putSerializable(DECISION, decision);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            decision = (Decision) getArguments().getSerializable(DECISION);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_card, container, false);
+
+        TextView title = (TextView) view.findViewById(R.id.title);
+        TextView description = (TextView) view.findViewById(R.id.description);
+        LinearLayout buttonContainer = (LinearLayout) view.findViewById(R.id.button_container);
+
+        title.setText("Decision");
+        description.setText(decision.getQuestion());
+
+        ArrayList<Button> answerButtons = new ArrayList<>();
+        for(int i = 0; i < decision.getAnswers().size(); i++){
+            buttonContainer.addView(makeSpace(1));
+
+            Button btn = makeAnswerButton(decision.getAnswers().get(i).getAnswerText(), 1);
+            answerButtons.add(btn);
+
+            buttonContainer.addView(btn);
+        }
+        buttonContainer.addView(makeSpace(1));
+
+        return view;
+    }
+
+    private Button makeAnswerButton(String text, int weight){
+        Button btn = new Button(this.getActivity());
+        btn.setText(text);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.weight = 1;
+        btn.setLayoutParams(lp);
+        return btn;
+    }
+
+    private Space makeSpace(int weight){
+        Space space = new Space(this.getActivity());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.weight = weight;
+        space.setLayoutParams(lp);
+        return space;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
